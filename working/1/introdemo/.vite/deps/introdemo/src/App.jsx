f573b0c4b4fcdd3cf9b35e1282be08fea1e8cedf
dom19740 <dbuzugbe@gmail.com>
2024-11-26 12:20:54 +0000

    1.1 done

--- working/1/introdemo/.vite/deps/introdemo/src/App.jsx
+++ working/1/introdemo/.vite/deps/introdemo/src/App.jsx
@@ -1,9 +1,27 @@
+const Hello = (props) => {
+
+  console.log(props)
+  return (
+    <div>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
-  const friends = [ 'Peter', 'Maya']
+
+  const name = 'Peter'
+  const age = 10
 
   return (
     <div>
-      <p>{friends}</p>
+      <h1>Greetings</h1>
+
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
